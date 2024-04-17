7dfc9d443632de0d47f81076572814769feb7e64
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-04-16 21:50:21 -0300

    Refactor: Reorganize functions within date_time component JavaScript file
    Restructured the JavaScript file associated with the date_time component by moving functions into the component for improved organization. This refactor enhances code encapsulation and readability, ensuring a clearer separation of concerns within the component.

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "./DateTime.css";
-import Search from "../Search/Search";
 
-export default function DateTime({ time, day }) {
+export default function DateTime({ weatherData, dayOrNight }) {
+  const [currentHour, setCurrentHour] = useState(false);
+  const [dayOfWeek, setDayOfWeek] = useState(false);
+
+  useEffect(() => {
+    const convertTime = (dt, timezone) => {
+      const timestamp = new Date(dt * 1000);
+      const localTime = timestamp.getTime();
+      const targetTime = localTime + timezone * 1000;
+      const targetDate = new Date(targetTime);
+
+      const hours = targetDate.getUTCHours().toString().padStart(2, "0");
+      const minutes = targetDate.getUTCMinutes().toString().padStart(2, "0");
+      const formattedTime = `${hours}:${minutes}`;
+      setCurrentHour(hours);
+
+      const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      const dayOfWeek = daysOfWeek[targetDate.getUTCDay()];
+
+      setCurrentHour(formattedTime);
+      setDayOfWeek(dayOfWeek);
+      dayOrNight(hours > 4 && hours < 18 ? true : false);
+    };
+
+    convertTime(weatherData.dt, weatherData.timezone);
+  }, [weatherData]);
+
   return (
-    <div className="display">
-      <div className="datetime">
-        <p className="time">{time}</p>
-        <p className="day">{day}</p>
-      </div>
-      <Search />
+    <div className="datetime">
+      <p className="time">{currentHour}</p>
+      <p className="currentDay">{dayOfWeek}</p>
     </div>
   );
 }
