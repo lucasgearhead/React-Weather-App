05e3372da606798ee5343545568954fd75d09e91
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-04-16 22:05:28 -0300

    Refactor: Simplify logic in app.jsx
    Streamlined the logic in the app.jsx file, making it more straightforward and easier to understand. Removed unnecessary complexity and refactored code for improved readability and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,84 @@
-import "./App.css";
+import React, { useState } from "react";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import DateTime from "./components/DateTime/DateTime";
+import Search from "./components/Search/Search";
+import Loading from "./components/Loading/Loading";
+import { ReactComponent as SunIcon } from "./assets/icons/weather/sun.svg";
+import { ReactComponent as MoonIcon } from "./assets/icons/weather/moon.svg";
+import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDay, setIsDay] = useState(false);
+
+  const fetchData = async (city) => {
+    setIsLoading(true);
+    try {
+      const weatherResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1871496f2ca4459545804dd8e8be545b&units=metric`
+      );
+      const wData = await weatherResponse.json();
+
+      if (wData.cod === 200) {
+        setWeatherData(wData);
+      } else {
+        alert(wData.message);
+      }
+    } catch (error) {
+      alert("Error fetching weather data:", error);
+    }
+
+    try {
+      const forecastResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=1871496f2ca4459545804dd8e8be545b&units=metric`
+      );
+
+      const fData = await forecastResponse.json();
+
+      if (fData.cod === "200") {
+        setForecastData(fData);
+        console.log(forecastData);
+      } else {
+        alert(fData.message);
+      }
+    } catch (error) {
+      alert("Error fetching weather data:", error);
+    }
+    setIsLoading(false);
+  };
+
+  const dayOrNight = (value) => {
+    setIsDay(value);
+  };
+
   return (
-    <div className="app">
-      <CurrentWeather
-        temp={"31"}
-        rain={"2%"}
-        humidity={"48%"}
-        wind={"11 km/h"}
-        weather={"Clear"}
-      />
-      <DateTime time={"18:52"} city={"sorocaba"} day={"Monday"} />
-    </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : weatherData == null ? (
+        <div className="chooseCity">
+          <h1>Weather</h1>
+          <Search handleCity={fetchData} />
+        </div>
+      ) : (
+        <div className={`${isDay ? "day" : "night"}`}>
+          <div className="app">
+            {isDay ? (
+              <SunIcon className="sun" />
+            ) : (
+              <MoonIcon className="moon" />
+            )}
+            <CurrentWeather weatherData={weatherData} />
+            <div>
+              <DateTime weatherData={weatherData} dayOrNight={dayOrNight} />
+              <Search handleCity={fetchData} weatherData={weatherData} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
