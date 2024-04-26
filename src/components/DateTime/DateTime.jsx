5950881d450c64fcba11c5b4e70cc39d6502dcf2
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-04-26 14:11:47 -0300

    refactor: Update DateTime component to use ConvertedTime utility
    Refactored the DateTime component to utilize the ConvertedTime utility function for time and day conversion. This update removes the dependency on weatherData and simplifies the component by directly obtaining currentDayOfWeek, dayOrNight, and currentHour from the utility function. The component now uses useEffect to update the day or night status and current day of the week based on the values returned by ConvertedTime.

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -1,49 +1,34 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect } from "react";
 import "./DateTime.css";
+import ConvertedTime from "../../utils/ConvertedTime";
 
 export default function DateTime({
   weatherData,
-  dayOrNight,
-  currentDayOfWeek,
-  returnDayOfWeek,
+  isDayOrNight,
+  theCurrentDayOfWeek,
 }) {
-  const [currentHour, setCurrentHour] = useState(false);
+  //const [currentHour, setCurrentHour] = useState(false);
+  const { currentDayOfWeek, dayOrNight, currentHour } = ConvertedTime(
+    weatherData.dt,
+    weatherData.timezone
+  );
 
   useEffect(() => {
-    const convertTime = (dt, timezone) => {
-      const timestamp = new Date(dt * 1000);
-      const localTime = timestamp.getTime();
-      const targetTime = localTime + timezone * 1000;
-      const targetDate = new Date(targetTime);
-
-      const hours = targetDate.getUTCHours().toString().padStart(2, "0");
-      const minutes = targetDate.getUTCMinutes().toString().padStart(2, "0");
-      const formattedTime = `${hours}:${minutes}`;
-      setCurrentHour(hours);
-
-      const daysOfWeek = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      const dayOfWeek = daysOfWeek[targetDate.getUTCDay()];
-
-      setCurrentHour(formattedTime);
-      currentDayOfWeek(dayOfWeek);
-      dayOrNight(hours > 4 && hours < 18 ? true : false);
-    };
-
-    convertTime(weatherData.dt, weatherData.timezone);
-  }, [dayOrNight, weatherData.dt, weatherData.timezone, currentDayOfWeek]);
+    isDayOrNight(dayOrNight);
+    theCurrentDayOfWeek(currentDayOfWeek);
+  }, [
+    weatherData,
+    isDayOrNight,
+    currentDayOfWeek,
+    dayOrNight,
+    currentHour,
+    theCurrentDayOfWeek,
+  ]);
 
   return (
     <div className="datetime">
       <p className="time">{currentHour}</p>
-      <p className="currentDay">{returnDayOfWeek}</p>
+      <p className="currentDay">{currentDayOfWeek}</p>
     </div>
   );
 }
