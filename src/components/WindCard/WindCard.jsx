cae1d1c5d43c97235e99488cce9aa874baa815d9
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-05-03 14:16:58 -0300

    docs: Add comments explaining wind card rendering logic in WindCard component

--- src/components/WindCard/WindCard.jsx
+++ src/components/WindCard/WindCard.jsx
@@ -4,24 +4,30 @@ import ConvertedTime from "../../utils/ConvertedTime";
 import ConvertedSpeed from "../../utils/ConvertedSpeed";
 
 export default function WindCard({ forecastData, unit }) {
+  // State variable to control whether to show wind cards
   const [showCards, setShowCards] = useState(false);
 
   useEffect(() => {
+    // Set showCards to true when component mounts
     setShowCards(true);
   }, []);
 
   return (
     <div className="wind-container">
+      {/* Map over forecast data to render wind cards */}
       {forecastData.list.slice(0, 8).map((data, index) => {
+        // Get current hour using ConvertedTime function
         const { currentHour } = ConvertedTime(
           data.dt,
           forecastData.city.timezone
         );
         return (
           <div key={index} className={`wind-card ${showCards ? "show" : ""}`}>
+            {/* Display wind speed using ConvertedSpeed function */}
             <p className="wind-speed">
               {ConvertedSpeed(data.wind.speed, unit).convertedSpeed}
             </p>
+            {/* Render arrow indicating wind direction */}
             <div
               className="arrow-container"
               style={{
@@ -31,6 +37,7 @@ export default function WindCard({ forecastData, unit }) {
               <div className="arrow"></div>
               <div className="body-arrow"></div>
             </div>
+            {/* Display current hour */}
             <p className="wind-speed">{currentHour}</p>
           </div>
         );
